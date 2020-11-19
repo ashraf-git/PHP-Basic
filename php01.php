6f505d151ad0638aae55896fc6a79b767e0d1105
ashraf-git <aliashraf.hena@gmail.com>
2020-11-19 15:38:12 +0600

    switch/case

--- php01.php
+++ php01.php
@@ -8,3 +8,40 @@ $r = $n % 2;
      default:
      echo "{$n} is a odd number";
  }
+
+ $color = 'blue';
+ switch($color){
+    case "red":
+        echo "Red is my favourite colour";
+        break;
+    case "green":
+        echo "Green is my favourite colour";
+        break;
+    case "blue":
+        echo "Blue is not my favourite colour";
+        break;
+    default:
+        echo "This color is ok";
+
+ }
+
+
+ $color = "green";
+ switch($color){
+     case "red":
+    //     echo "Red is my favourite colour";
+    //     break;
+    //  case "green":
+    //     echo "Green is my favourite colour";
+     case "green":
+        echo ucwords($color). "is my favourite colour";
+        break;
+     default:
+     echo "This colour is ok";
+ }
+ echo "\n";
+$color = "yellow";
+ if ("red" == $color || "green" == $color) {
+     echo ucwords($color). " is my favourite colour";
+ }else{
+     echo "this is ok";  }
