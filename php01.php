7d75bd32cfcce09eab9d34446d767ae8652e3a9e
ashraf-git <aliashraf.hena@gmail.com>
2020-11-19 19:26:41 +0600

    ternary operator

--- php01.php
+++ php01.php
@@ -1,42 +1,4 @@
 <?php
-$n = 12;
-if($n%2 == 0){
-    echo "even number";
-}else{
-    echo "odd number";
-}
-echo PHP_EOL;
-
-if($n%2 == 0):
-    echo "even number";
-    echo PHP_EOL;
-    echo "some text";
-  
-else: echo "odd number";
-    echo PHP_EOL;
-    echo "some text ++";
-endif;
-
-echo PHP_EOL;
-switch($n%2==0):
-    case 0:
-        echo "even number";
-        break;
-        default:
-        echo "odd number";
-    endswitch;
-    echo PHP_EOL;
-  
-
-    $n = 15;
-if ($n % 2 == 0):
-    ?>
-    even number
-    some text
-    <?php
-    else: 
-        ?>
-        odd number
-        some text ++
-        <?php
-        endif;
+$n = 10;
+$result = (($n % 2 == 0) ? "A" : ($n == 11)) ? "B" : "C";
+echo $result;
