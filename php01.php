e6ce2663264fd77c52662b9633776cfcc16f1bc4
ashraf-git <aliashraf.hena@gmail.com>
2020-11-19 19:49:15 +0600

    various kinds of loop

--- php01.php
+++ php01.php
@@ -1,4 +1,26 @@
 <?php
-$n = 10;
-$result = (($n % 2 == 0) ? "A" : ($n == 11)) ? "B" : "C";
-echo $result;
+for($i=1;$i<=10;$i+=2){
+    //echo $i;
+    echo PHP_EOL;
+    for($j=0;$j<$i; $j++){ 
+    echo "*"; 
+    }
+}
+echo PHP_EOL;
+$i = 0;
+while($i<10){
+    $i++;
+    echo $i.PHP_EOL;
+}
+echo PHP_EOL;
+$i = 0;
+do{
+    $i++;
+    echo $i.PHP_EOL;
+}while($i<10);
+
+echo "Go to \n";
+$i=0;
+a: $i++;
+echo $i.PHP_EOL;
+if($i<10)goto a;
