804db66812b6f9eba2b20d1f0641b5a346ac121c
ashraf-git <aliashraf.hena@gmail.com>
2020-11-19 16:44:52 +0600

    switch/case/even/odd

--- php01.php
+++ php01.php
@@ -1,47 +1,42 @@
 <?php
-$n = 12;
-$r = $n % 2;
- switch($r){
-     case 0:
-     echo "{$n} is a even number\n";
-     break;
-     default:
-     echo "{$n} is a odd number";
- }
+     $n = -13;
+     $r = $n % 2;
 
- $color = 'blue';
- switch($color){
-    case "red":
-        echo "Red is my favourite colour";
+     switch($r){
+         case 0:
+            switch($n){
+                case $n> 0: 
+                    echo "$n is a positve even number";
+                break;
+                case $n < 0: 
+                    echo "$n is a negative even number";
+                break;
+            }
         break;
-    case "green":
-        echo "Green is my favourite colour";
-        break;
-    case "blue":
-        echo "Blue is not my favourite colour";
-        break;
-    default:
-        echo "This color is ok";
+         default:
+            switch($n){
+                case $n > 0: 
+                    echo "$n is a positve odd number";
+                break;
+                case $n < 0: 
+                    echo "$n is a negative odd number";
+                break;
+            }
+     }
 
- }
+     echo "\n";
+     switch(true){
+         case (0 == $r && $n > 0):
+            echo "$n is a positive even number";
+            break;
+         case (1 == $r && $n >  0):
+            echo "$n is a positive odd number";
+            break;
+         case (0 == $r && $n < 0):
+            echo "$n is a negative even number";
+            break;
+         case (-1 == $r && $n <  0):
+            echo "$n is a negative odd number";
+            break;
 
-
- $color = "green";
- switch($color){
-     case "red":
-    //     echo "Red is my favourite colour";
-    //     break;
-    //  case "green":
-    //     echo "Green is my favourite colour";
-     case "green":
-        echo ucwords($color). "is my favourite colour";
-        break;
-     default:
-     echo "This colour is ok";
- }
- echo "\n";
-$color = "yellow";
- if ("red" == $color || "green" == $color) {
-     echo ucwords($color). " is my favourite colour";
- }else{
-     echo "this is ok";  }
+     }
