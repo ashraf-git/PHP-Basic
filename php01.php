d94732d18c3de80113327f78b49a3f8f9e52a67d
ashraf-git <aliashraf.hena@gmail.com>
2020-11-19 11:56:13 +0600

    breack

--- php01.php
+++ php01.php
@@ -1,29 +1,10 @@
 <?php
-$condition1 = true;
-$condition2 = true;
-$condition3 = false;
-
-if($condition1){
-    if($condition2){
-        if($condition3){
-            echo "Hello";
-        }else{
-            echo "no 1";
-        }
-    }else{
-        echo "no 2";
-    }
-}else{
-    echo "no 3";
-}
-echo"\n";
-
-if ($condition1 && $condition2 && $condition3) {
-    echo "Hello";
-}else if ($condition1 && $condition2){
-    echo "no 1";
-}elseif($condition1){
-    echo "no 2";
-}else{
-    echo"no 3";
-}
+$n = 12;
+$r = $n % 2;
+ switch($r){
+     case 0:
+     echo "{$n} is a even number\n";
+     break;
+     default:
+     echo "{$n} is a odd number";
+ }
