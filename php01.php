a311214e582d767c9d469b9b1e29966f641e7a68
ashraf-git <aliashraf.hena@gmail.com>
2020-11-20 14:43:35 +0600

    for loop multiple steping

--- php01.php
+++ php01.php
@@ -1,26 +1,8 @@
 <?php
-for($i=1;$i<=10;$i+=2){
-    //echo $i;
-    echo PHP_EOL;
-    for($j=0;$j<$i; $j++){ 
-    echo "*"; 
-    }
-}
-echo PHP_EOL;
-$i = 0;
-while($i<10){
-    $i++;
-    echo $i.PHP_EOL;
-}
-echo PHP_EOL;
-$i = 0;
-do{
-    $i++;
-    echo $i.PHP_EOL;
-}while($i<10);
+// for($i = 10; $i >= 1; $i--){
+//     echo $i.":".(11-$i).PHP_EOL;
+// }
 
-echo "Go to \n";
-$i=0;
-a: $i++;
-echo $i.PHP_EOL;
-if($i<10)goto a;
+for($i = 10, $j =1; $i> 0; $i--,$j++){ 
+    echo $i.":" .$j.PHP_EOL;
+}
