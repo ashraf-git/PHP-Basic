472e0a6149e5f303ba60d34c9e9534a795b2bd40
ashraf-git <aliashraf.hena@gmail.com>
2020-11-21 11:39:13 +0600

    space ship operator

--- php01.php
+++ php01.php
@@ -1,10 +1,30 @@
 <?php
-$varyold = 0;
-$old = 1;
-$new = 1;
-for($i = 0; $i < 10; $i++) {
-    echo $varyold." ";
-    $old = $new;
-    $new = $old + $varyold;
-    $varyold = $old;
+$x = 7;
+$y = 6;
+
+function greaterOrSmaller($x, $y){
+    if($x>$y){
+        return 1;
+    }elseif($x==$y){
+        return 0;
+    }else{
+        return -1;
+    }
+}
+if(greaterOrSmaller($x,$y) == 1){ 
+    echo "{$x} is greater than {$y}";
+}else if(greaterOrSmaller($x,$y) == 0){
+    echo "{$x} is equal to {$y}";
+}else if(greaterOrSmaller($x,$y) == -1){
+    echo "{$x} is less than {$y}";
+}
+
+echo PHP_EOL;
+$result = $x<=>$y;
+if($result==1){
+    echo "{$x} is greater than {$y}";
+}else if($result==0){
+    echo "{$x} is equal to {$y}";
+}else if($result==-1){
+    echo "{$x} is less than {$y}";
 }
