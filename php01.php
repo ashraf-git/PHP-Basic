854c49e10b2c1c50bb1256d1a02cdd3030232b31
ashraf-git <aliashraf.hena@gmail.com>
2020-11-21 13:07:32 +0600

    null colesce operator

--- php01.php
+++ php01.php
@@ -1,30 +1,14 @@
 <?php
-$x = 7;
-$y = 6;
+$default_lat = 23.9;
+$default_lon = 90.8;
+$user_lat = 12.9;
 
-function greaterOrSmaller($x, $y){
-    if($x>$y){
-        return 1;
-    }elseif($x==$y){
-        return 0;
-    }else{
-        return -1;
-    }
-}
-if(greaterOrSmaller($x,$y) == 1){ 
-    echo "{$x} is greater than {$y}";
-}else if(greaterOrSmaller($x,$y) == 0){
-    echo "{$x} is equal to {$y}";
-}else if(greaterOrSmaller($x,$y) == -1){
-    echo "{$x} is less than {$y}";
-}
+// $lat = isset($user_lat) ? $user_lat : $default_lat;
+$lat = $user_lat ?? $default_lat;
+echo $lat;
 
-echo PHP_EOL;
-$result = $x<=>$y;
-if($result==1){
-    echo "{$x} is greater than {$y}";
-}else if($result==0){
-    echo "{$x} is equal to {$y}";
-}else if($result==-1){
-    echo "{$x} is less than {$y}";
+if(isset($user_lat)){
+    $lat = $user_lat;
+}else{
+    $lat = $default_lat;
 }
