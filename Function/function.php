c0fbc41023990b3e7d4fcc10fc2309ce8d455544
ashraf-git <aliashraf.hena@gmail.com>
2020-11-21 13:51:12 +0600

    function parameter type check

--- Function/function.php
+++ Function/function.php
@@ -1,9 +1,5 @@
 <?php
 include_once "functions.php";
 
-$x = 11;
-if(isEven($x)){
-    echo "{$x} is an even number";
-}else{
-    echo "{$x} is an Odd number";
-}
+$x = "i";
+echo "the facrotial of {$x} is " . factorial($x);
--- Function/functions.php
+++ Function/functions.php
@@ -5,3 +5,14 @@ function isEven($n){
     }
     return false;
 }
+
+function factorial(int $n){
+    // if(gettype($n)!="integer"){
+    //     return "invalid";
+    // }
+    $result = 1;
+    for($i = $n; $i> 1; $i--){
+        $result *= $i;
+    }
+    return $result;
+}
